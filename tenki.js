5a1246464b344601e036b251a28921d0fda55e66
naoki-Yamakawa <n0629yyyyy@yahoo.co.jp>
2025-05-03 15:10:48 +0900

    Update tenki.js

--- tenki.js
+++ tenki.js
@@ -1,7 +1,6 @@
 async function getWeather() {
   const city = document.getElementById("cityInput").value;
-  const apiKey = "YOUR_API_KEY"; // ←ここに後でAPIキーを入れる！
-
+  const apiKey = "YOUR_API_KEY"; // ← 自分のOpenWeatherのAPIキーをここに入れる
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ja`;
 
   const res = await fetch(url);
@@ -9,14 +8,13 @@ async function getWeather() {
 
   if (res.ok) {
     document.getElementById("weatherResult").innerHTML = `
-        <h2>${data.name}の天気</h2>
-        <p>気温：${data.main.temp} ℃</p>
-        <p>湿度：${data.main.humidity} %</p>
-        <p>天気：${data.weather[0].description}</p>
+        <p>場所: ${data.name}</p>
+        <p>気温: ${data.main.temp}℃</p>
+        <p>天気: ${data.weather[0].description}</p>
+        <p>湿度: ${data.main.humidity}%</p>
       `;
   } else {
-    document.getElementById(
-      "weatherResult"
-    ).innerHTML = `<p>都市が見つかりませんでした</p>`;
+    document.getElementById("weatherResult").innerHTML =
+      "都市が見つかりませんでした。";
   }
 }
